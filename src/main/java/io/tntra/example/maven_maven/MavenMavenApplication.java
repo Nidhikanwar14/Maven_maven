14dddbebaf6473166679c609b2c93c08ba1e5df9
Nidhi <nidhikanwar45@gmail.com>
2022-08-05 17:44:21 +0530

    javaScript_maven

---
+++ src/main/java/io/tntra/example/maven_maven/MavenMavenApplication.java
@@ -0,0 +1,13 @@
+package io.tntra.example.maven_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/tntra/example/maven_maven/MavenMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tntra.example.maven_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
